ec5a5488a19aa5efc32e7ffa156eebc291dc2d08
chiaraforzoni <chiara.forzoni@mail.polimi.it>
2017-02-27 03:29:22 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 function setup() {
   
-  createCanvas(800,800);
-  frameRate(20)
+  createCanvas(windowWidth,windowHeight);
+  frameRate(20);
 
 }
 
@@ -13,8 +13,8 @@ if (random()< 0.5){
     fill("#ffed62");
   }
   
- for(var y=0; y<820; y+=40){
-    for(var x=0; x<820; x+=20){
+ for(var y=0; y<windowHeight; y+=40){
+    for(var x=0; x<windowWidth; x+=20){
       triangle(x,y, x+20, y, x+10, y+20);  }  }
 
 
@@ -23,13 +23,13 @@ if (random()< 0.5){
     fill("#ffed62");
   
 
-  for(var Y=0; Y<820; Y+=40){
-    for (var X=0; X<820; X+=20){
+  for(var Y=0; Y<windowHeight; Y+=40){
+    for (var X=0; X<windowWidth; X+=20){
       triangle(X-10, Y+20, X+10, Y+20, X+0, Y+40); } }
       
       
 textAlign(CENTER);
-textSize(24);
+textSize(34);
 noStroke()
 fill(0);
 text("To prevent an epilettic attack click anywhere", windowWidth/2, windowHeight/2);
@@ -44,3 +44,6 @@ if (mouseIsPressed){
 
 }
 
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
